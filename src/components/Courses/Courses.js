fdc6cee3c9cdb6c43fe3856f71abaee60c6ad0c9
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-10-04 03:42:46 +0600

    courses page done Alhamdulillah

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
 
 const Courses = () => {
+    const [ courses, setCourses ] = useState( [] );
+
+    useEffect( () => {
+        fetch( './courses.json' )
+            .then( res => res.json() )
+            .then( data => setCourses( data ) );
+    }, [] );
+
+    console.log( courses );
+
     return (
-        <div>
-            <h1>This is our Courses Page</h1>
+        <div className="row row-cols-1 row-cols-md-3 g-5 m-5">
+            {
+                courses.map( course =>
+                    <div className="col">
+                        <div className="card h-100 w-75 p-3 rounded-3">
+                            <img src={ course.image } className="card-img-top img-fluid" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title fw-bold text-primary">{ course.name }</h5>
+                                <p className="card-text">{ course.description }</p>
+                                <h3 className="card-text fw-bold">Price: ${ course.price }</h3>
+                            </div>
+                            <div className="card-footer">
+                                <Button className="w-100">Course Details</Button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     );
 };
