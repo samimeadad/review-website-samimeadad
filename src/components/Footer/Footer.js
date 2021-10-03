e7c1cc915ef93a960d0b50ebf2cdf8398a195632
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-10-04 02:22:48 +0600

    footer section done Alhamdulillah

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is Footer</h1>
-        </div>
+        <footer className="bg-dark text-center p-5">
+            <h1 className="text-info"><span className="text-warning">SAMI'S</span> EDUCARE</h1>
+            <h6 className="text-info">LEARN FOR PASSION, NOT FOR PROFESSION</h6>
+            <p className="text-info">Follow Us On:</p>
+            <p>
+                <a href="https://www.facebook.com/" target="_blank"><i aria-hidden="true" className="fa fa-facebook text-warning me-3" data-toggle="tooltip" title="Facebook"></i></a>
+                <a href="https://www.twitter.com/" target="_blank"><i aria-hidden="true" className="fa fa-twitter text-warning me-3" data-toggle="tooltip" title="Twitter"></i></a>
+                <a href="https://www.linkedin.com/" target="_blank"><i aria-hidden="true" className="fa fa-linkedin text-warning me-3" data-toggle="tooltip" title="Linkedin"></i></a>
+                <a href="https://www.github.com/" target="_blank"><i aria-hidden="true" className="fa fa-github text-warning me-3" data-toggle="tooltip" title="Github"></i></a>
+            </p>
+            <p className="text-secondary">&copy;Sami Meadad Choudhury</p>
+        </footer>
     );
 };
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,28 +4,30 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand>
-                <NavLink to="/home" className="ms-3 me-5 text-info text-decoration-none">
-                    Sami's Educare
-                </NavLink>
-            </Navbar.Brand>
-            <Nav className="me-auto">
-                <NavLink className="me-3 text-info text-decoration-none" to="/home">Home</NavLink>
-                <NavLink className="me-3 text-info text-decoration-none" to="/courses">Courses</NavLink>
-                <NavLink className="me-3 text-info text-decoration-none" to="/teachers">Teachers</NavLink>
-                <NavLink className="me-3 text-info text-decoration-none" to="/about">About Us</NavLink>
-            </Nav>
-            <Form className="d-flex">
-                <FormControl
-                    type="search"
-                    placeholder="Search Course Here"
-                    className="me-2"
-                    aria-label="Search"
-                />
-                <Button className="me-3" variant="outline-info">Search</Button>
-            </Form>
-        </Navbar>
+        <header>
+            <Navbar className="p-3" bg="dark" variant="dark">
+                <Navbar.Brand>
+                    <NavLink to="/home" className="me-5 text-info text-decoration-none">
+                        Sami's Educare
+                    </NavLink>
+                </Navbar.Brand>
+                <Nav className="me-auto">
+                    <NavLink className="me-3 text-info text-decoration-none" to="/home">Home</NavLink>
+                    <NavLink className="me-3 text-info text-decoration-none" to="/courses">Courses</NavLink>
+                    <NavLink className="me-3 text-info text-decoration-none" to="/teachers">Teachers</NavLink>
+                    <NavLink className="me-3 text-info text-decoration-none" to="/about">About Us</NavLink>
+                </Nav>
+                <Form className="d-flex">
+                    <FormControl
+                        type="search"
+                        placeholder="Search Course Here"
+                        className="me-2"
+                        aria-label="Search"
+                    />
+                    <Button variant="outline-info">Search</Button>
+                </Form>
+            </Navbar>
+        </header>
     );
 };
 
