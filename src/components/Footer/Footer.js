4174faebcf2b0cc4e177e8fefe091da906a7502c
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-10-04 02:28:08 +0600

    anchor tag warning fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,10 +7,10 @@ const Footer = () => {
             <h6 className="text-info">LEARN FOR PASSION, NOT FOR PROFESSION</h6>
             <p className="text-info">Follow Us On:</p>
             <p>
-                <a href="https://www.facebook.com/" target="_blank"><i aria-hidden="true" className="fa fa-facebook text-warning me-3" data-toggle="tooltip" title="Facebook"></i></a>
-                <a href="https://www.twitter.com/" target="_blank"><i aria-hidden="true" className="fa fa-twitter text-warning me-3" data-toggle="tooltip" title="Twitter"></i></a>
-                <a href="https://www.linkedin.com/" target="_blank"><i aria-hidden="true" className="fa fa-linkedin text-warning me-3" data-toggle="tooltip" title="Linkedin"></i></a>
-                <a href="https://www.github.com/" target="_blank"><i aria-hidden="true" className="fa fa-github text-warning me-3" data-toggle="tooltip" title="Github"></i></a>
+                <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><i aria-hidden="true" className="fa fa-facebook text-warning me-3" data-toggle="tooltip" title="Facebook"></i></a>
+                <a href="https://www.twitter.com/" target="_blank" rel="noreferrer"><i aria-hidden="true" className="fa fa-twitter text-warning me-3" data-toggle="tooltip" title="Twitter"></i></a>
+                <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer"><i aria-hidden="true" className="fa fa-linkedin text-warning me-3" data-toggle="tooltip" title="Linkedin"></i></a>
+                <a href="https://www.github.com/" target="_blank" rel="noreferrer"><i aria-hidden="true" className="fa fa-github text-warning me-3" data-toggle="tooltip" title="Github"></i></a>
             </p>
             <p className="text-secondary">&copy;Sami Meadad Choudhury</p>
         </footer>
