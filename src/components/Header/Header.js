f74a650d0ebbe5496a93f868f6bb2a4ead55c402
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-10-04 11:36:39 +0600

    home header section done Alhamdulillah

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Form, FormControl, Nav, Navbar, Button, NavDropdown, Row, Col } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-import coverImage from '../../images/cover.jpg'
 
 const Header = () => {
     return (
@@ -38,23 +37,6 @@ const Header = () => {
                     <Button variant="outline-info">Search</Button>
                 </Form>
             </Navbar>
-
-            <section className='m-5'>
-                <Row>
-                    <Col xs={ 12 } md={ 7 }>
-                        <img className="img-fluid" src={ coverImage } alt="coverImage" />
-                    </Col>
-                    <Col xs={ 6 } md={ 5 }>
-                        <div className="mt-5 p-5 me-5 h-75 text-start bg-dark rounded-3">
-                            <h1 className="text-info mb-3">Sami's <span className="text-warning fw-bold">Educare</span></h1>
-                            <hr className="text-info" />
-                            <h2 className="text-warning">Academic <span className="text-info fw-bold">Excellence,</span> Educational <span className="text-info fw-bold">Innovation,</span> Social <span className="text-info fw-bold">Justice.</span></h2>
-                            <hr className="text-info" />
-                            <h3 className="me-3 mb-4 text-warning">Learn for <span className="text-info fw-bold">PASSION</span>, not for <span className="text-info fw-bold">PROFESSION</span></h3>
-                        </div>
-                    </Col>
-                </Row>
-            </section>
         </header>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import coverImage from '../../images/cover.jpg';
 
 const Home = () => {
     return (
         <div className="m-5">
+            <header className='m-5'>
+                <Row>
+                    <Col xs={ 12 } md={ 7 }>
+                        <img className="img-fluid" src={ coverImage } alt="coverImage" />
+                    </Col>
+                    <Col xs={ 6 } md={ 5 }>
+                        <div className="mt-5 p-5 me-5 h-75 text-start bg-dark rounded-3">
+                            <h1 className="text-info mb-3">Sami's <span className="text-warning fw-bold">Educare</span></h1>
+                            <hr className="text-info" />
+                            <h2 className="text-warning">Academic <span className="text-info fw-bold">Excellence,</span> Educational <span className="text-info fw-bold">Innovation,</span> Social <span className="text-info fw-bold">Justice.</span></h2>
+                            <hr className="text-info" />
+                            <h3 className="me-3 mb-4 text-warning">Learn for <span className="text-info fw-bold">PASSION</span>, not for <span className="text-info fw-bold">PROFESSION</span></h3>
+                        </div>
+                    </Col>
+                </Row>
+            </header>
             <h1 className="text-center text-info bg-dark p-3 rounded-3">Our Premium Courses</h1>
         </div>
     );
